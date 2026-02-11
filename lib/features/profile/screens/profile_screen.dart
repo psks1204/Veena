f00770550340f1f8035ace876b51de5880765e23
Jonesmith <jonesmith199309@gmail.com>
2026-02-11 14:43:17 -0500

    bug fixes

--- lib/features/profile/screens/profile_screen.dart
+++ lib/features/profile/screens/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../../core/theme/app_spacing.dart';
