bacb50a7ab800c6aa764f904af7c684077dd6b00
Jonesmith <jonesmith199309@gmail.com>
2026-02-12 00:00:40 -0500

    Web build fixes

--- lib/features/player/screens/unified_player_screen.dart
+++ lib/features/player/screens/unified_player_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:ui';
 import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide RepeatMode;
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:video_player/video_player.dart';
--- lib/shared/widgets/full_player.dart
+++ lib/shared/widgets/full_player.dart
@@ -1,5 +1,5 @@
 import 'dart:ui';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide RepeatMode;
 import '../../../core/theme/app_colors.dart';
 import '../../../core/theme/app_spacing.dart';
 import 'package:provider/provider.dart';
